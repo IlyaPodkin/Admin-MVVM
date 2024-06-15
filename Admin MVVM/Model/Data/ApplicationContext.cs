70f2fda9650dd99d5aac2e15b1a0138f1f723425
IlyaPodkin <ilhapodkin@yandex.ru>
2024-06-15 15:00:39 +0400

    Добавление EF Core в проект, добавление класса пользователь

---
+++ Admin MVVM/Model/Data/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin_MVVM.Model.Data
+{
+    class ApplicationContext : DbContext
+    {
+        //Добавление данных в таблицу Users
+        public DbSet<User> Users { get; set; } = null!;
+
+        public ApplicationContext() 
+        {
+            // если БД нет, то этот метод создаст её
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Строка подключения БД
+            optionsBuilder.UseSqlite("Data Source=Admin.db");
+        }
+
+    }
+}
---
+++ Admin MVVM/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public User(Guid id, string name, string email, string password)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+}
+
